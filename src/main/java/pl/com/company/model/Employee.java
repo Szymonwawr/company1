dd014fc6419cdd0e261c706ed2e66a459fdec655
Szywawr <wawrzkiewiczszymon4@gmail.com>
2022-06-25 15:26:40 +0200

    init

---
+++ src/main/java/pl/com/company/model/Employee.java
@@ -0,0 +1,90 @@
+package pl.com.company.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Employee {
+
+
+    private final LocalDateTime createAt;
+    private final LocalDateTime updatedAt;
+
+
+    private String firstName;
+    private String lastName;
+    private final String pesel;
+    private BigDecimal salary;
+
+    
+
+    public Employee(String firstName, String lastName, String pesel, BigDecimal salary ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.pesel = pesel;
+
+        this.createAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+
+
+
+    public Employee expected(String firstNameTest2, String lastNameTest2, BigDecimal salaryTest2, String peselTest) {
+        return null;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(createAt, employee.createAt) && Objects.equals(updatedAt, employee.updatedAt) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, pesel, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", createAt=" + createAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
+
+}
+
+
---
+++ src/test/java/pl/com/company/repository/EmployeeRepoTest.java
@@ -0,0 +1,61 @@
+package pl.com.company.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class EmployeeRepoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void create() {
+
+    }
+
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void getSalary() {
+    }
+
+    @Test
+    void getCreateAt() {
+    }
+
+    @Test
+    void getUpdatedAt() {
+    }
+
+    @Test
+    void getPesel() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
